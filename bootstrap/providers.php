9e0375d4ad6ac86cdf00da9e97d7cfb31be8549e
Bashir Mahad <basheirmahad@gmail.com>
2025-05-23 07:41:48 +0300

    Change: Provider.php

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,5 +1,12 @@
 <?php
 
 return [
+
     App\Providers\AppServiceProvider::class,
 ];
+
+
+// This file is part of the Laravel framework.
+// (c) Laravel LLC <https://laravel.com>
+// For the full copyright and license information, please view the LICENSE
+// file that was distributed with this source code.
